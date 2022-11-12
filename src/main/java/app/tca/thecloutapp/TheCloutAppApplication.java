331a7f8d7ec28e6974be472ed5007ff99847ca0e
Obi James Onyemaechi <jamesobi@softprism.org>
2022-11-13 00:50:39 +0100

    Auth implemented

---
+++ src/main/java/app/tca/thecloutapp/TheCloutAppApplication.java
@@ -0,0 +1,13 @@
+package app.tca.thecloutapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCloutAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCloutAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/tca/thecloutapp/TheCloutAppApplicationTests.java
@@ -0,0 +1,13 @@
+package app.tca.thecloutapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCloutAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
